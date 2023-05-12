4fe2c1b5b8520f7f2fbe00f6cefef3eaaccf1768
VisheshThapa <visheshjthapa@gmail.com>
2023-05-12 13:41:02 -0700

    Dotfiles

---
+++ .config/nvim/lua/plugins/goConfig.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+      })
+    end,
+  },
+}
